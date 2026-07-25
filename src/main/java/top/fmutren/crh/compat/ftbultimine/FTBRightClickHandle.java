274c2cdaa027c328f29db5bd1000df3a857ed89f
FMutren <fmutren@gmail.com>
2026-07-25 22:15:55 +0800

    Fixed the wrench when use with ftbultimine

--- src/main/java/top/fmutren/crh/compat/ftbultimine/FTBRightClickHandle.java
+++ src/main/java/top/fmutren/crh/compat/ftbultimine/FTBRightClickHandle.java
@@ -4,12 +4,17 @@ import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import dev.ftb.mods.ftbultimine.api.rightclick.RegisterRightClickHandlerEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.event.level.BlockEvent;
 
+import static com.simibubi.create.content.equipment.wrench.IWrenchable.playRemoveSound;
 import static top.fmutren.crh.interaction.StateSwitch.iterationTypeForItem;
 import static top.fmutren.crh.interaction.TryToEncase.tryToEncaseAllType;
 import static top.fmutren.crh.interaction.util.ChainOperation.centerHit;
@@ -39,9 +44,9 @@ public class FTBRightClickHandle {
                                     player,
                                     hand,
                                     heldItem,
-                                    centerHit(pos, face));
+                                    centerHit(pos, face.getOpposite()));
                             if (player.isShiftKeyDown()) {
-                                wrenchable.onSneakWrenched(state, useOnContext);
+                                onSneakWrenched(state, useOnContext);
                             } else {
                                 wrenchable.onWrenched(state, useOnContext);
                             }
@@ -60,5 +65,32 @@ public class FTBRightClickHandle {
             }
             return count;
         }));
+    }
+
+    public static InteractionResult onSneakWrenched(BlockState state, UseOnContext context) {
+        {
+            Level world = context.getLevel();
+            BlockPos pos = context.getClickedPos();
+            Player player = context.getPlayer();
+
+            if (world instanceof ServerLevel serverLevel) {
+
+                BlockState blockState = serverLevel.getBlockState(pos);
+                new BlockEvent.BreakEvent(world, pos, blockState, player);
+
+                if (player != null && !player.isCreative()) {
+                    Block.getDrops(state, serverLevel, pos, world.getBlockEntity(pos), player, context.getItemInHand())
+                            .forEach(itemStack -> {
+                                player.getInventory()
+                                        .placeItemBackInInventory(itemStack);
+                            });
+                }
+
+                state.spawnAfterBreak(serverLevel, pos, ItemStack.EMPTY, true);
+                world.destroyBlock(pos, false);
+                playRemoveSound(world, pos);
+            }
+            return InteractionResult.SUCCESS;
+        }
     }
 }
